a7d1605e5950b0ce77b1e1f2edb040b6a4ec585c
Jason Ramos <jasonramos3004@gmail.com>
2026-01-13 10:57:28 +0530

    updated csv

--- scrape.py
+++ scrape.py
@@ -12,12 +12,28 @@ tables = soup.find_all('table', class_='stats_table')[0]##only first table
 links = tables.find_all('a')
 links =[l.get("href") for l in links]##link example: en/squads/18bb7c10/Arsenal-Stats
 links = [l for l in links if '/squads' in l]##use only link to squads, discard others
-links = [l for l in links if 'Wolv' in l]
 
 team_urls = [f"https://fbref.com{l}" for l in links]##make sure links are complete by adding the https prefix
 print(team_urls)
-all_teams =[]
-for team_url in team_urls:
+
+team_url = team_urls[0]
+name = team_url.split('/')[-1]##example - Arsenal-Stats
+name = name.replace("-Stats","")
+team_response = cureq.get(team_url,impersonate="chrome")
+soup = BeautifulSoup(team_response.text,'lxml')
+squad = soup.find_all('table',class_ = "stats_table")[0]
+print(squad.columns)
+if squad and squad.columns:
+    squad.columns = squad.columns.droplevel()
+    
+##squad is a BeautifulSoup tag   
+##convert to dataframe
+team_data = pd.read_html(str(squad))[0]
+team_data["Team"] = name
+team_data.to_csv('fpl.csv', mode='a', header=not os.path.exists('fpl.csv'))
+time.sleep(10)
+
+for team_url in team_urls[1:]:
     name = team_url.split('/')[-1]##example - Arsenal-Stats
     name = name.replace("-Stats","")
     team_response = cureq.get(team_url,impersonate="chrome")
@@ -31,6 +47,7 @@ for team_url in team_urls:
     team_data = pd.read_html(str(squad))[0]
     team_data["Team"] = name
     team_data.to_csv('fpl.csv', mode='a', header=False)
+    time.sleep(10)
     
 
 
